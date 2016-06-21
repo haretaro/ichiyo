194d71d4387ca6583f4050087e5c815fbd58fa8a
Eisaku <13ec083@ms.dendai.ac.jp>
2016-06-21 14:41:37 +0900

    3系に対応(コマンド)、保存バグ修正

--- twitter/TwitterScraping.py
+++ twitter/TwitterScraping.py
@@ -7,18 +7,31 @@ import calendar
 import argparse
 import datetime
 
+python_ver = 2
+
 def dformat(date):
     return str(date.year) + "-" + str(date.month) + "-" + str(date.day)
 
+def getCalendar_month(month):
+    month_array = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    return month_array[month - 1]
+
 def sformat(date):
-    return str(date.day) + " " + calendar.month_abbr[date.month] + " " + str(date.year)
+    return str(date.day) + " " + getCalendar_month(date.month) + " " + str(date.year)
 
 def extractData(data, tdata, date_source):
     date = datetime.datetime.strptime(date_source,"%Y-%m-%d")
+    until = date + datetime.timedelta(days=1)
     r = ["date:data"]
     
     for (e1, e2) in zip(data, tdata):
-        if sformat(date) == e2.text:
+        tmp = e2.find_all("span")
+        t = tmp[0]
+        t2 = t['data-time']
+        tmp2 = datetime.datetime.fromtimestamp(int(t2))
+        
+        if tmp2 > date and until > tmp2:
+        #if sformat(date) == e2.text:
             r.append(date_source + " :" + e1.text.replace("\n", " ").encode('utf-8'))
     return r
 
@@ -28,7 +41,7 @@ def writeData(data, tdata, word, date_source):
         f = open("output_"+ word + "_" + date_source +".txt","w")
         r = extractData(data, tdata, date_source)
         for e in r:
-            print(e + "\n")
+            #print(e + "\n")
             f.write(e + "\n")
         f.close()
         print("Finish word: " + word +" date:" + date_source)
@@ -48,7 +61,10 @@ def Ichiyo(word, since, until , interval):
 
 def TWscraping(word, date_source, interval):
     date = datetime.datetime.strptime(date_source,"%Y-%m-%d")
-    url = urllib.quote(str(word) + " since:" + dformat(date - datetime.timedelta(days=1)) + " until:" + dformat(date + datetime.timedelta(days=1)))
+    if python_ver == 3:
+        url = urllib.parse.quote(str(word) + " since:" + dformat(date - datetime.timedelta(days=1)) + " until:" + dformat(date + datetime.timedelta(days=1)))
+    else:
+        url = urllib.quote(str(word) + " since:" + dformat(date - datetime.timedelta(days=1)) + " until:" + dformat(date + datetime.timedelta(days=1)))
     browser = webdriver.Firefox()
     browser.get("https://twitter.com/search?q=" + url)
     
@@ -79,7 +95,10 @@ if __name__ == "__main__":
     parser.add_argument("until", help=u"")
     parser.add_argument("-d", dest="date",help="example: 2015-8-12")
     parser.add_argument("-i", dest="interval", default=2, help=u"Update interval: Short if it fails do not cry.")
+    parser.add_argument("-p", dest="pyversion", default=2, help="python version default 2")
     args = parser.parse_args()
+    
+    python_ver = args.pyversion
     
     #TWscraping(args.word, args.date, args.interval)
     Ichiyo(args.word, args.since, args.until, args.interval)
